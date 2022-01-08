ae71a8d26c4989a7936433e9bd63910eab686d87
Jovick-coder <victorjosiahm3@gmail.com>
2022-01-08 18:05:18 +0100

    made folder structure

---
+++ src/components/nav/nav.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import  logoImg from '../../assets/images/logo.png'
+
+const NavComponent = () => {
+    return ( <>
+    <div>
+    <img src={logoImg}/>
+    </div>
+    </> );
+}
+ 
+export default NavComponent;
