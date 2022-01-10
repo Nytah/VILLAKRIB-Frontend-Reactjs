f4f5b05251db84e3b733f3ae2da55e936ec7defe
unknown <jjatuah@gmail.com>
2022-01-10 18:46:49 +0100

    65% of the work for the featuredAgent component completed

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -6,6 +6,11 @@ import image3 from "../../assets/images/image 6.png";
 import image1 from "../../assets/images/image 4.png";
 import image2 from "../../assets/images/image 7.png";
 import image4 from "../../assets/images/image-.png";
+import Agent1 from "../../assets/images/Agent1.png";
+import Agent2 from "../../assets/images/Agent2.png";
+import Agent3 from "../../assets/images/Agent3.png";
+import FeaturedAgent from "../../components/featuredAgent/featuredAgent.component";
+
 
 
 const HomePage = () => {
@@ -22,6 +27,17 @@ const HomePage = () => {
           <PropCategory image={image2} text="House & Villa"/>
           <PropCategory image={image3} text="Offices"/>
           <PropCategory image={image4} text="Furnished Appartments"/>
+      </div>
+
+      <div className="ourFeaturedAgents">
+        <h1>Our Featured Agents</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do Ut enim ad minim veniam.</p>
+      </div>
+
+      <div className="featuredAgent">
+        <FeaturedAgent image={Agent1} location="" name="John Doe"/>
+        <FeaturedAgent image={Agent2} location="" name="John Doe"/>
+        <FeaturedAgent image={Agent3} location="" name="John Doe"/>
       </div>
    </>);
 };
