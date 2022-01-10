8c00d6b8ecfd958150cf36a846c70d73b1640373
Jovick-coder <victorjosiahm3@gmail.com>
2022-01-10 08:21:49 +0100

    working on the nav bar

--- src/components/nav/nav.component.jsx
+++ src/components/nav/nav.component.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
-import  logoImg from '../../assets/images/logo.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import logoImg from "../../assets/images/logo.png";
+import "./nav.style.css";
 
 const NavComponent = () => {
-    return ( <>
-    <div>
-    <img src={logoImg}/>
-    </div>
-    </> );
-}
- 
+  return (
+    <>
+      <div id="nav">
+        <div className="logo-div">
+          <img src={logoImg} alt="" />
+        </div>
+        <div className="link-div">
+          <ul>
+            <li>
+              <Link to="">Hi</Link>{" "}
+            </li>
+            <li>
+              <Link to="">Hi</Link>{" "}
+            </li>
+            <li>
+              <Link to="">Hi</Link>{" "}
+            </li>
+            <li>
+              <Link to="">Hi</Link>{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default NavComponent;
