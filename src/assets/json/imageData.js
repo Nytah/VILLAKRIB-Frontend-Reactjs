e68704a6306f7817e3da23aef7067c4021830a5c
unknown <jjatuah@gmail.com>
2022-01-22 18:34:21 +0100

    added images

--- src/assets/json/imageData.js
+++ src/assets/json/imageData.js
@@ -2,7 +2,19 @@ import image1 from "../../assets/images/slide1.jpg";
 import image2 from "../../assets/images/slide2.jpg";
 import image3 from "../../assets/images/slide3.jpg";
 import image4 from "../../assets/images/1.jpeg";
-
+import Agent01 from "../images/Agent01.png";
+import Agent02 from "../images/Agent02.png";
+import Agent03 from "../images/Agent03.png";
+import Agent04 from "../images/Agent04.png";
+import Agent05 from "../images/Agent05.png";
+import Agent06 from "../images/Agent06.png";
+import Agent07 from "../images/Agent07.png";
+import Agent08 from "../images/Agent08.png";
+import Agent09 from "../images/Agent09.png";
+import Agent10 from "../images/Agent10.png";
+import Agent11 from "../images/Agent11.png";
+import Agent12 from "../images/Agent12.png";
+import Agent13 from "../images/Agent13.png";
 
 // this datas feeds the carousel on the hero section
 export const CarouselImages = [
@@ -62,3 +74,138 @@ export const PropertyListingsImage = [
         textHeader: 'lorem Header 44',
     },
 ]
+
+// This data is for the featured agents section
+
+export const agentData = {
+    agent1: {
+      name: "Louis Vuitton",
+      location: "Kaduna State",
+      value: "10 mill",
+      image: Agent01,
+      facebook: "",
+      instagram: "",
+      phone: "",
+      email: ""
+    },
+    agent2: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent02,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+    },
+    agent3: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent03,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent4: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent04,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent5: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent05,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent6: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent06,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent7: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent07,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent8: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent08,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent9: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent09,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent10: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent10,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent11: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent11,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent12: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent12,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+      agent13: {
+        name: "Louis Vuitton",
+        location: "Kaduna State",
+        value: "10 mill",
+        image: Agent13,
+        facebook: "",
+        instagram: "",
+        phone: "",
+        email: ""
+      },
+  }
