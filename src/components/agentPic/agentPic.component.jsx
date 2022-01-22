8f9ed3d00867b114d8c2fab0a2daf1ba31f814a5
unknown <jjatuah@gmail.com>
2022-01-22 17:07:05 +0100

    first commit

---
+++ src/components/agentPic/agentPic.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AgentPic = () => {
+  return ( 
+    <div>
+      
+    </div>
+   );
+}
+ 
+export default AgentPic;
---
+++ src/components/featuredAgents/featuredAgents.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FeaturedAgents = () => {
+  return ( 
+    <div>
+      
+    </div>
+   );
+}
+ 
+export default FeaturedAgents;
