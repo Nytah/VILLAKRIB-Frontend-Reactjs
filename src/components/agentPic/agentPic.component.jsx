0cbe237f235ee58bca33b893e51b1de5389d1aaa
unknown <jjatuah@gmail.com>
2022-01-24 14:35:11 +0100

    styled the agent pic component

--- src/components/agentPic/agentPic.component.jsx
+++ src/components/agentPic/agentPic.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./agentPic.style.css";
 import { agentData } from '../../assets/json/imageData';
 
 const AgentPic = ({image}) => {
--- src/components/featuredAgents/featuredAgents.component.jsx
+++ src/components/featuredAgents/featuredAgents.component.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import AgentPic from '../agentPic/agentPic.component';
 import AgentCard from '../agentCard/agentCard.component';
 import { agentData } from '../../assets/json/imageData';
+import "./featuredAgents.style.css";
 
 
 
 const FeaturedAgents = () => {
   return ( 
     <div className='featuredAgents'>
-      <AgentPic image={agentData.agent1.image} />
-      <AgentPic image={agentData.agent2.image} />
-      <AgentPic image={agentData.agent3.image} />
-      <AgentPic image={agentData.agent4.image} />
-      <AgentPic image={agentData.agent5.image} />
-      <AgentPic image={agentData.agent6.image} />
-      <AgentPic image={agentData.agent7.image} />
-      <AgentPic image={agentData.agent8.image} />
-      <AgentPic image={agentData.agent9.image} />
-      <AgentPic image={agentData.agent10.image} />
-      <AgentPic image={agentData.agent11.image} />
-      <AgentPic image={agentData.agent12.image} />
-      <AgentPic image={agentData.agent13.image} />
+     <div className="agentPictures">
+        <AgentPic image={agentData.agent1.image} />
+        <AgentPic image={agentData.agent2.image} />
+        <AgentPic image={agentData.agent3.image} />
+        <AgentPic image={agentData.agent4.image} />
+        <AgentPic image={agentData.agent5.image} />
+        <AgentPic image={agentData.agent6.image} />
+        <AgentPic image={agentData.agent7.image} />
+        <AgentPic image={agentData.agent8.image} />
+        <AgentPic image={agentData.agent9.image} />
+        <AgentPic image={agentData.agent10.image} />
+        <AgentPic image={agentData.agent11.image} />
+        <AgentPic image={agentData.agent12.image} />
+        <AgentPic image={agentData.agent13.image} />
+     </div>
     </div>
    );
 }
